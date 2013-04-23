dfe6afa751c162e0d877e9ee875a0c76bf5eba9b
bingyangzeng <binmes@126.com>
2013-04-23 23:31:30 +0800

    1st

---
+++ upload/system/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => '2w@p2',
+  'dbname' => 'pua',
+);
+$config['slave'] = false;
